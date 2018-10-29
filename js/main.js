5c17951b035609020b2a167e5995271bc38b7429
hebale <hebale@naver.com>
2018-10-29 12:38:07 +0900

    181029_update
    size confirm required

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 var count = 0;
 
+var imgAlt,	// gallery 이미지의 alt 값 반환
+	imgLen,	// gallery 이미지의 alt 값의 length 반환
+	imgNum,	// gallery 이미지의 alt 값의 length의 뒷 번호 num으로 반환
+	modalImg,	// click된 이미지의 modal 이미지의 부모자 선택
+	modalNavImg;	//click된 이미지의 modal nav 이미지의 부모자 선택
+
 $(function(){
-	
+
+
+
+	//------------------------------------------------------------------
+	//-------------------- jeep gallery page jquery --------------------
+	//------------------------------------------------------------------
 	$(".thumb_right_btn").click(function(){
 		if(count <= 1){
 			count++;
@@ -9,7 +20,7 @@ $(function(){
 			thumbMove();
 		}
 	});
-
+	
 	$(".thumb_left_btn").click(function(){
 		if(count >= 0){
 			count--;
@@ -17,13 +28,59 @@ $(function(){
 			thumbMove();
 		}
 	});
+
+	$(".thumb_nav>li").click(function(e){
+		e.preventDefault();
+		$(".thumb_left_btn").css("display","none");
+		$(".thumb_right_btn").css("display","none");
+
+		imgAlt = $(this).find("img").attr("alt");
+		imgLen = imgAlt.length;
+		imgNum = parseInt(imgAlt.substr(imgLen-2,2));
+		modalImg = $(".modal_photo").find("[alt =" + imgAlt + "]").parent();
+		modalNavImg = $(".modal_nav").find("[alt =" + imgAlt + "]").parents("li");
+		
+		//console.log(imgNum);
+		//console.log(imgAlt);
+		
+		$("#modal").css("display","block");
+
+		$(".modal_photo>div").css("display","none");
+		modalImg.css("display","block");
+		modalNavImg.addClass("on");
+
+		$(".modal_nav>ul").animate({left: 720+ (imgNum * 240 * -1)})
+	});
+
+	$(".modal_nav>ul>li").click(function(){
+
+		imgAlt = $(this).find("img").attr("alt");
+		imgLen = imgAlt.length;
+		imgNum = parseInt(imgAlt.substr(imgLen-2,2));
+		modalImg = $(".modal_photo").find("[alt =" + imgAlt + "]").parent();
+		modalNavImg = $(".modal_nav").find("[alt =" + imgAlt + "]").parents("li");
+
+		$(".modal_photo>div").css("display","none");
+		modalImg.css("display","block");
+
+		$(".modal_nav>ul>li").removeClass();
+		modalNavImg.addClass("on");
+
+		$(".modal_nav>ul").animate({left: 720+ (imgNum * 240 * -1)})
+	})
+
+	// 
+	// parseInt("03"); 이미지 alt 순번 리스트 num로 가져오기
+
+	$("button").click(function(){
+			$("#modal").css("display","none");
+
+			$(".thumb_left_btn").css("display","block");
+			$(".thumb_right_btn").css("display","block");
+	})
 });
 
 function thumbMove(){
 	var x = count * 1200 * -1;
 	$("#view_point>div").animate({left:x},500);
 };
-
-function modalPage(){
-	
-}
